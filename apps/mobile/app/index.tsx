51677986799fb0e418f35047bf05a41d1da1298b
nguyennguyen0 <trungnguyenwork123@gmail.com>
2025-09-04 09:08:26 +0700

    basic setup expo react native app with nativewind. and demo use type from @repo/types

---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex items-center justify-center">
+      <Text className="text-2xl text-amber-300 font-bold">Zolara</Text>
+    </View>
+  );
+}
