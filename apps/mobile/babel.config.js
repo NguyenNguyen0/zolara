4277cb4a13dc1a73d9c348c1e4cd6ecb8925531f
nguyennguyen0 <trungnguyenwork123@gmail.com>
2025-10-24 11:00:00 +0700

    feat: Update app configuration and build settings, add environment variables and permissions for eas, expo

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -12,6 +12,7 @@ module.exports = function (api) {
                 {
                     alias: {
                         "@": ".",
+						"@repo/types": "../../packages/types"
                     },
                 },
             ],
