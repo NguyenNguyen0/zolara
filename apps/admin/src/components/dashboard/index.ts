e2a432dc15cce1af0b32b02dce76e64a9fc6d8da
nvminh162 <nvminh162@gmail.com>
2025-11-13 11:05:17 +0700

    remove mono project

--- apps/admin/src/components/dashboard/index.ts
+++
@@ -1,4 +0,0 @@
-export { OverviewSection } from './OverviewSection';
-export { AnalyticsSection } from './AnalyticsSection';
-export { FirebaseAnalyticsSection } from './FirebaseAnalyticsSection';
-export { DashboardSidebar, type DashboardSection } from './DashboardSidebar';
--- apps/admin/src/contexts/index.ts
+++
@@ -1,7 +0,0 @@
-// Export context types and contexts
-export { AuthContext, type AuthContextType, type User } from './AuthContextType';
-export { DashboardContext, type DashboardContextType } from './DashboardContextType';
-
-// Export provider components
-export { AuthProvider } from './AuthContext';
-export { DashboardProvider } from './DashboardContext';
